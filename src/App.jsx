26efd67c9be7822a78e0c2db049202a61e9af83f
ertyhd <orionship@protonmail.com>
2023-01-31 12:47:42 +0200

    Refactoring to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,55 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import MainMenu from './components/MainMenu/MainMenu';
 import Section from './components/Section/Section';
 import Statistics from 'components/Statistics/Statistics';
 
-// import './App.css';
-
 const options = ['good', 'bad', 'neutral'];
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [feeds, setFeeds] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const handleFeedback = feedback => {
+    setFeeds(prevState => {
+      const value = prevState[feedback];
+      return { ...prevState, [feedback]: value + 1 };
+    });
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feeds;
     return good + bad + neutral;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    let totalFeedback = this.countTotalFeedback();
-    const { good } = this.state;
+  const countPositiveFeedbackPercentage = () => {
+    let totalFeedback = countTotalFeedback();
+    const { good } = feeds;
     return totalFeedback ? Math.round((good / totalFeedback) * 100) : 0;
   };
 
-  // handleFeedback = ({ target }) => {
-  //   const { feedback } = target.dataset;
-  //   this.setState(prevState => {
-  //     return { [feedback]: prevState[feedback] + 1 };
-  //   });
-  // };
+  const { good, neutral, bad } = feeds;
+  const totalFeedbackCount = countTotalFeedback();
+  const positiveFeedback = countPositiveFeedbackPercentage();
 
-  handleFeedback = feedback => {
-    this.setState(prevState => ({
-      [feedback]: prevState[feedback] + 1,
-    }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedbackCount = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <MainMenu options={options} actionFeedback={this.handleFeedback} />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={totalFeedbackCount}
-            positiveFeedbackPercentage={positiveFeedback}
-          />
-        </Section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <MainMenu options={options} actionFeedback={handleFeedback} />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedbackCount}
+          positiveFeedbackPercentage={positiveFeedback}
+        />
+      </Section>
+    </>
+  );
+};
 
 export default App;
